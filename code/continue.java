5e2e409901f313c14566b7bc101fb55541006668
FabianSuter <suter117@gmail.com>
2024-11-12 11:25:28 +0100

    Vorl 2 added

---
+++ code/continue.java
@@ -0,0 +1,12 @@
+// Zahlen aufsummieren, die grösser 0 sind
+int[] numbers = { -5, 4, 2, -7, 4 };
+int sum = 0;
+
+for (int x : numbers) {
+    if (x < 0) {
+        System.out.println("Verworfen: " + x);
+        continue;
+    }
+    sum += x;
+}
+System.out.println("Summe: " + sum);
