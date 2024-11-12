f954b05451a841a83581a304277b0dd8f2a3b961
FabianSuter <suter117@gmail.com>
2024-11-12 13:38:11 +0100

    Vorl4 added

---
+++ code/Weekday.java
@@ -0,0 +1,8 @@
+public enum Weekday{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    public boolean isWeekend(){
+        return this == SATURDAY || this == SUNDAY;
+    }
+}
+Weekday currentDay = ...
+if (currentDay.isWeekend()){...}
