06d5c3d54ad35acd29b0acdaaad09d4fcbe39200
FabianSuter <suter117@gmail.com>
2025-01-31 15:54:25 +0100

    07_Streams angepasst

--- code/serializing.java
+++ code/serializing.java
@@ -1,14 +1,11 @@
 var person = new Person();
 
 // Objekt serialisieren
-try (var stream = new ObjectOutputStream(
+try(var stream = new ObjectOutputStream(
                     new FileOutputStream("serial.bin"))) {
-    stream.writeObject(person);
-}
+    stream.writeObject(person);}
 
 // Objekt deserialisieren
-try (var stream = new ObjectInputStream(
+try(var stream = new ObjectInputStream(
                     new FileInputStream("serial.bin"))) {
-    Person p = (Person)stream.readObject();
-    // ...
-}
+    Person p = (Person)stream.readObject();}
